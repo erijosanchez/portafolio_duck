f85da5b9a5ef15560e7a17186bb4b03a8a31eac1
Erick José Sanchez Pinedo <erijito.2@gmail.com>
2024-12-23 00:48:02 -0500

    Estilos del header

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,12 +1,18 @@
-<header class="header">
-    <div class="container nav">
-        <h2>Erick José Sanchez Pinedo</h2>
-        <ul class="nav-links">
-            <li><a href="#inicio">Inicio</a></li>
-            <li><a href="#sobre-mi">Sobre M&#xed;</a></li>
-            <li><a href="#habilidades">Habilidades</a></li>
-            <li><a href="#proyectos">Proyectos</a></li>
-            <li><a href="#contacto">Contacto</a></li>
-        </ul>
-    </div>
-</header>
+<div class="container-h">
+    <header class="header">
+        <div class="container nav">
+            <div class="logo">
+                <img src="build/assets/frontend/img/Captura_de_pantalla_2024-11-15_020010-removebg-preview.png" alt="">
+                <h1>Erick José Sanchez Pinedo</h1>
+            </div>
+            
+            <ul class="nav-links">
+                <li><a href="#inicio">Inicio</a></li>
+                <li><a href="#sobre-mi">Sobre M&#xed;</a></li>
+                <li><a href="#habilidades">Habilidades</a></li>
+                <li><a href="#proyectos">Proyectos</a></li>
+                <li><a href="#contacto">Contacto</a></li>
+            </ul>
+        </div>
+    </header>
+</div>
